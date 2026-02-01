ac9e8a703e41b3c1eaef967603431400aebbb643
Sorady Sun <sundante70@gmail.com>
2026-02-01 13:28:41 +0700

    Fix dashboard redirect page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,5 @@
 import { redirect } from "next/navigation";
-import PatientsTable from "../../components/PatientsTable";
 
 export default function DashboardIndex() {
   redirect("/dashboard/patients");
 }
-<button className="btn btnPrimary">＋ New Patient</button>
